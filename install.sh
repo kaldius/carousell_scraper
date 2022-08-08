0792327d4489b467f7e7d0b23eae2e2f95b2d09b
Quan Teng Foong (uif08811) <uif08811@igl5755u.conti.de>
2022-08-08 22:57:15 +0800

    improve install and reset scripts

--- install.sh
+++ install.sh
@@ -5,24 +5,30 @@ read -p "Please key in your telegram bot token: " token
 echo "$token" > ./config/token.txt
 
 # install python dependencies
+echo "Creating python virtual environment and installing dependencies..."
 python -m venv scraper_venv
 source scraper_venv/bin/activate
 pip install -r requirements.txt
 
-# create cron script
+# create cron scripts
+echo "Creating cron scripts..."
 echo "source $PWD/scraper_venv/bin/activate && python $PWD/scrape_once.py" > cron_scrape.sh
 echo "source $PWD/scraper_venv/bin/activate && python $PWD/main.py" > start_bot.sh
 
-# write out current crontab
-crontab -l > mycron
-# echo new cron into cron file
-echo "0,10,20,30,40,50 * * * * bash $PWD/cron_scrape.sh >> $PWD/scraper_logs.log 2>&1" >> mycron
-echo "@reboot bash $PWD/start_bot.sh >> $PWD/bot_logs.log 2>&1" >> mycron
-# install new cron file
-crontab mycron
-rm mycron
-
-read -p "carousell_scraper requires a reboot to complete installation. Reboot now? (y/n) " answer
+read -p "Do you want to set the recommended cron jobs? (y/n) " answer
 if [ "$answer" == "y" ]; then
-    reboot
+    echo "Setting cron jobs..."
+    # write out current crontab
+    crontab -l > mycron
+    # echo new cron into cron file
+    echo "0,10,20,30,40,50 * * * * bash $PWD/cron_scrape.sh >> $PWD/scraper_logs.log 2>&1" >> mycron
+    echo "@reboot bash $PWD/start_bot.sh >> $PWD/bot_logs.log 2>&1" >> mycron
+    # install new cron file
+    crontab mycron
+    rm mycron
+
+    read -p "A reboot is required before the cron jobs can be started. Reboot now? (y/n) " answer
+    if [ "$answer" == "y" ]; then
+        reboot
+    fi
 fi
--- reset.sh
+++ reset.sh
@@ -3,11 +3,21 @@
 read -p "Delete token? (y/n) " answer
 if [ "$answer" == "y" ]; then
     rm ./config/token.txt
+    echo "Token deleted."
+fi
+
+read -p "Delete data? (y/n) " answer
+if [ "$answer" == "y" ]; then
+    rm -rf ./data
+    echo "Data deleted."
 fi
 
 rm -r scraper_venv
+echo "Python virtual environment deleted."
 rm cron_scrape.sh
 rm start_bot.sh
+echo "Cron scripts deleted."
 
 crontab -l | grep -v "cron_scrape.sh" | crontab -
 crontab -l | grep -v "start_bot.sh" | crontab -
+echo "Cron jobs deleted."
