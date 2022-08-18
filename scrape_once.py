b036acbada25f1ddb1788de289c5dc7246656720
kaldius <quantf@gmail.com>
2022-08-18 17:58:39 +0800

    fix bug in scrape_once.py

--- scrape_once.py
+++ scrape_once.py
@@ -2,15 +2,22 @@ import csv
 from config.definitions import ROOT_DIR
 from scraper import scraper
 import os
+import json
+
+def extract_all_search_terms(monitored_searches):
+    search_terms = []
+    for user_id in monitored_searches:
+        for search_term in monitored_searches[user_id]["searches"]:
+            search_terms.append(search_term)
+    return search_terms
 
 if __name__ == "__main__":
-    load_path = os.path.join(ROOT_DIR, "data", "monitored_searches.csv")
+    load_path = os.path.join(ROOT_DIR, "data", "monitored_searches.json")
     if os.path.exists(load_path):
-        with open(load_path, "r") as f:
-            reader = csv.reader(f)
-            monitored_searches = {int(row[0]): row[1:] for row in reader}
+        with open(load_path, "r", encoding="utf-8") as f:
+            monitored_searches = json.load(f)
             print(monitored_searches)
-            for list_of_items in monitored_searches.values():
-                for item in list_of_items:
-                    print("\nScraping for: " + item)
-                    scraper.search(item)
+            all_search_terms = extract_all_search_terms(monitored_searches)
+            for item in all_search_terms:
+                print("\nScraping for: " + item)
+                scraper.search(item)
